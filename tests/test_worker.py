ae1971b23135a65653f8d11e9f9f1824cfa88632
Tharindu <tharindu.nayanajith.madusanka@gmail.com>
2021-03-26 22:01:49 +0530

    Update test_worker.py output_path

--- tests/test_worker.py
+++ tests/test_worker.py
@@ -5,7 +5,7 @@ from    thumb_gen.worker  import  Generator
 def test_worker():
     #video_path, output_path='', custom_text='True', font_dir='', font_size=0
     video_path = "/home/sample-mp4-file.mp4"
-    output_path = "/home/travis/build/truethari/thumb-gen/"
+    output_path = "/home/runner/work/thumb-gen/thumb-gen/"
     custom_text = "thumb gen"
     app = Generator(video_path, output_path, custom_text)
     test_worker_value = app.run()
