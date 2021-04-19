242d2af05b42c16b42e6036da5a26cae5e746bc7
Tharindu N <40334396+truethari@users.noreply.github.com>
2021-04-19 19:17:33 +0530

    Update tests

--- tests/test_worker.py
+++ tests/test_worker.py
@@ -1,12 +1,19 @@
 import pytest
 
-from thumb_gen.worker   import Generator
+from thumb_gen   import Generator
 
-def test_worker():
-    #video_path, output_path='', custom_text='True', font_dir='', font_size=0
+#video_path, output_path='', custom_text=True, font_dir='', font_size=0, bg_colour='', font_colour=''
+
+def test_worker_1():
     video_path = "/home/sample-mp4-file.mp4"
     output_path = "/home/runner/work/thumb-gen/thumb-gen/"
-    custom_text = "thumb gen"
-    app = Generator(video_path, output_path, custom_text)
+    app = Generator(video_path, output_path, "thumb gen")
+    test_worker_value = app.run()
+    assert test_worker_value == True
+
+def test_worker_2():
+    video_path = "/home/sample-mp4-file.mp4"
+    output_path = "/home/runner/work/thumb-gen/thumb-gen/"
+    app = Generator(video_path, output_path, font_size=10, bg_colour="yellow", font_colour="black")
     test_worker_value = app.run()
     assert test_worker_value == True
