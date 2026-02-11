211559ba0c7081369c15b5825304251f16f0f4ec
0xdilshan <257371012+0xdilshan@users.noreply.github.com>
2026-02-11 21:18:13 +0200

    Read the version without importing the package

--- setup.py
+++ setup.py
@@ -1,9 +1,16 @@
 import pathlib
+import os
 
 from setuptools import setup, find_packages
-from thumb_gen  import __version__
 
 here = pathlib.Path(__file__).parent.resolve()
+
+# Read the version safely without importing the package
+version_dict = {}
+with open(os.path.join(here, "thumb_gen", "version.py")) as fp:
+    exec(fp.read(), version_dict)
+__version__ = version_dict["__version__"]
+
 long_description = (here / 'README.md').read_text(encoding='utf-8')
 
 setup(
